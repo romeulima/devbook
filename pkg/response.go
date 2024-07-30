39784aab8f2e268d7d8d5e17d0c35fa2bfa31135
Romeu Lima <romeu.sousadp@gmail.com>
2024-07-30 18:01:06 -0300

    feat: creating json default response function

---
+++ pkg/response.go
@@ -0,0 +1,12 @@
+package pkg
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func WriteJSON(w http.ResponseWriter, s int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(s)
+	return json.NewEncoder(w).Encode(v)
+}
